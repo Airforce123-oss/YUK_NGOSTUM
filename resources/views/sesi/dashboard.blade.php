6c948a13ff22ef06be96f5e93b5e9c7f57855d82
Muh. Haikal Hanis <75037509+Airforce123-oss@users.noreply.github.com>
2024-04-16 01:37:31 +0800

    edit file dashboard.blade

--- resources/views/sesi/dashboard.blade.php
+++ resources/views/sesi/dashboard.blade.php
@@ -35,7 +35,6 @@
             
         </div>
         <div class="text-white">
-            <!-- Ganti dengan ikon profil yang sesuai -->
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 14l7-7m2 7l7-7" />
