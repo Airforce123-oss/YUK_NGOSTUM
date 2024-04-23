d19b5f8591c87a4ac867b835d1b83e6e3332d0fc
Muh. Haikal Hanis <75037509+Airforce123-oss@users.noreply.github.com>
2024-04-23 11:19:08 +0700

    dashboard penyewa

---
+++ resources/views/Penyewa/dashboard-penyewa.blade.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
+    @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+
+    <meta name="generator" content="Hugo 0.122.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <title>Dashboard</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbars/">
+   
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<style>
+    body {
+  padding-bottom: 20px;
+}
+
+.navbar {
+  margin-bottom: 20px;
+  
+}
+
+@media screen and (max-width: 800px) {
+    .wrapper {
+        width: 90%;
+
+    }
+}
+
+
+</style>
+<body>
+<main>
+
+      <!-- Header Section -->
+    <nav class="p-4 flex justify-between items-center" style="background-color:#038175 ">
+        <div class="text-white text-lg font-bold" style="font-size: 30px; "><span style="color: red;">Yuk</span><span style="color: white;">Ngostum </span></div>
+        </div>
+        <div class="text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 14l7-7m2 7l7-7" />
+            </svg>
+        </div>
+    </nav>
+
+    <div class="h-80 flex justify-center items-center" style="background-image: url(/images/background.png)">
+        <div class="text-center text-[#038175]"><h2>Lagi Ada Event Cosplay?, Bingung Sewa Baju?, di Yuk Ngostum Aja Simpel dan Gercep</h2> </div>
+    </div>
+
+    <div class="rounded-[10px] bg-[#D9D9D9] absolute left-[79px] top-[300px] right-[110px] flex flex-row p-[39px_44px_39px_45px] box-sizing-border">
+        <div class="rounded-[5px] bg-[#FFFFFF] relative m-[0_23px_0_0] flex flex-row justify-center p-[10px_0_10px_0] h-[fit-content] grow basis-[201px] box-sizing-border">
+          <div class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#3B434F]">
+            Semua Kota
+          </div>
+        </div>
+        <div class="rounded-[5px] bg-[#FFFFFF] relative m-[0_23px_0_0] flex flex-row justify-center p-[10px_0_10px_0] h-[fit-content] grow basis-[201px] box-sizing-border">
+          <div class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#3B434F]">
+            Kategori
+          </div>
+        </div>
+        <div class="rounded-[5px] bg-[#FFFFFF] relative m-[0_23px_0_0] flex flex-row justify-center p-[10px_0_10px_0] h-[fit-content] grow basis-[201px] box-sizing-border">
+          <div class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#3B434F]">
+            Lama Sewa
+          </div>
+        </div>
+        <div class="rounded-[5px] bg-[#FFFFFF] relative m-[0_12.1px_0_0] flex flex-row justify-center p-[10px_0_10px_0] h-[fit-content] grow basis-[201px] box-sizing-border">
+          <div class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#3B434F]" placeholder="Rp. 0">
+            Rp. 0
+          </div>
+        </div>
+        <div class="m-[10px_11.1px_10px_0] inline-block break-words font-['Poppins'] font-semibold text-[15px] leading-[1.355] text-[#3B434F]">
+          -
+        </div>
+        <div class="rounded-[5px] bg-[#FFFFFF] relative m-[0_9px_0_0] flex flex-row justify-center p-[10px_0_10px_0] h-[fit-content] grow basis-[201px] box-sizing-border">
+          <div class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#3B434F]">
+            Rp. 500.000
+          </div>
+        </div>
+        <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[10px_7.8px_10px_7.8px] grow basis-[47px] box-sizing-border">
+          <button class="break-words font-['Poppins'] font-medium text-[15px] leading-[1.355] text-[#FFFFFF]" href="" type="submit" role="button">
+            Cari
+          </button>
+        </div>
+      </div>
+
+    
+      <div class="h-screen flex justify-center items-center rounded-lg" style="background-color: #fdf4dc; margin-top:30px;">
+        <div class="h-90 flex items-center" style="margin-top: -660px;">
+            <div>
+              <div class="rounded-[15px] bg-[#D9D9D9] relative m-[0_0_19px_4px] flex flex-row justify-between p-[11px_60px_11px_59px] w-[1800px] box-sizing-border" style="background-color: #D9D9D9">
+                <div class="m-[8px_0_4px_0] flex flex-row justify-between w-[631.1px] h-[fit-content] box-sizing-border">
+                    <div class="m-[1px_100px_1px_40px] inline-block w-[229.5px] break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Cahaya Konoha 
+                    </div>
+                    <div class="m-[1px_200px_1px_40px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Anime
+                    </div>
+                    <div class="m-[2px_200px_0_200x] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      3 Hari
+                    </div>
+                    <div class="m-[0_100px_2px_200px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Rp. 300.000
+                    </div>
+                    <div class="rounded-[5px] bg-[#219BE4] relative m-[0_200px_2px_200px] p-[6px_20px_8px_20px] w-[188px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]" style="white-space: nowrap;">
+                          Lihat Costume
+                        </span>
+                      </div>
+                      <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[6px_40px_8px_40px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]">
+                          Sewa
+                        </span>
+                      </div>
+           
+                  </div>
+              </div>
+              <div class="rounded-[15px] bg-[#D9D9D9] relative m-[0_0_19px_4px] flex flex-row justify-between p-[11px_60px_11px_59px] w-[1800px] box-sizing-border" style="background-color: #D9D9D9">
+                <div class="m-[8px_0_4px_0] flex flex-row justify-between w-[631.1px] h-[fit-content] box-sizing-border">
+                    <div class="m-[1px_100px_1px_40px] inline-block w-[229.5px] break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Cahaya Konoha
+                    </div>
+                    <div class="m-[1px_200px_1px_40px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Anime
+                    </div>
+                    <div class="m-[2px_200px_0_200x] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      3 Hari
+                    </div>
+                    <div class="m-[0_100px_2px_200px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Rp. 300.000
+                    </div>
+                    <div class="rounded-[5px] bg-[#219BE4] relative m-[0_200px_2px_200px] p-[6px_20px_8px_20px] w-[188px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]" style="white-space: nowrap;">
+                          Lihat Costume
+                        </span>
+                      </div>
+                      <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[6px_40px_8px_40px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]">
+                          Sewa
+                        </span>
+                      </div>
+                  </div>
+              </div>
+              <div class="rounded-[15px] bg-[#D9D9D9] relative m-[0_0_19px_4px] flex flex-row justify-between p-[11px_60px_11px_59px] w-[1800px] box-sizing-border" style="background-color: #D9D9D9">
+                <div class="m-[8px_0_4px_0] flex flex-row justify-between w-[631.1px] h-[fit-content] box-sizing-border">
+                    <div class="m-[1px_100px_1px_40px] inline-block w-[229.5px] break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Cahaya Konoha
+                    </div>
+                    <div class="m-[1px_200px_1px_40px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Anime
+                    </div>
+                    <div class="m-[2px_200px_0_200x] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      3 Hari
+                    </div>
+                    <div class="m-[0_100px_2px_200px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Rp. 300.000
+                    </div>
+                    <div class="rounded-[5px] bg-[#219BE4] relative m-[0_200px_2px_200px] p-[6px_20px_8px_20px] w-[188px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]" style="white-space: nowrap;">
+                          Lihat Costume
+                        </span>
+                      </div>
+                      <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[6px_40px_8px_40px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]">
+                          Sewa
+                        </span>
+                      </div>
+                  </div>
+              </div>
+              <div class="rounded-[15px] bg-[#D9D9D9] relative m-[0_0_19px_4px] flex flex-row justify-between p-[11px_60px_11px_59px] w-[1800px] box-sizing-border" style="background-color: #D9D9D9">
+                <div class="m-[8px_0_4px_0] flex flex-row justify-between w-[631.1px] h-[fit-content] box-sizing-border">
+                    <div class="m-[1px_100px_1px_40px] inline-block w-[229.5px] break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Cahaya Konoha 
+                    </div>
+                    <div class="m-[1px_200px_1px_40px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Anime
+                    </div>
+                    <div class="m-[2px_200px_0_200x] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      3 Hari
+                    </div>
+                    <div class="m-[0_100px_2px_200px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Rp. 300.000
+                    </div>
+                    <div class="rounded-[5px] bg-[#219BE4] relative m-[0_200px_2px_200px] p-[6px_20px_8px_20px] w-[188px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]" style="white-space: nowrap;">
+                          Lihat Costume
+                        </span>
+                      </div>
+                      <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[6px_40px_8px_40px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]">
+                          Sewa
+                        </span>
+                      </div>
+           
+                  </div>
+              </div>
+              <div class="rounded-[15px] bg-[#D9D9D9] relative m-[0_0_19px_4px] flex flex-row justify-between p-[11px_60px_11px_59px] w-[1800px] box-sizing-border" style="background-color: #D9D9D9">
+                <div class="m-[8px_0_4px_0] flex flex-row justify-between w-[631.1px] h-[fit-content] box-sizing-border">
+                    <div class="m-[1px_100px_1px_40px] inline-block w-[229.5px] break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Cahaya Konoha 
+                    </div>
+                    <div class="m-[1px_200px_1px_40px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Anime
+                    </div>
+                    <div class="m-[2px_200px_0_200x] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      3 Hari
+                    </div>
+                    <div class="m-[0_100px_2px_200px] inline-block break-words font-['Poppins'] font-semibold text-[20px] leading-[1.355] text-[#3B434F]" style="white-space: nowrap;">
+                      Rp. 300.000
+                    </div>
+                    <div class="rounded-[5px] bg-[#219BE4] relative m-[0_200px_2px_200px] p-[6px_20px_8px_20px] w-[188px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]" style="white-space: nowrap;">
+                          Lihat Costume
+                        </span>
+                      </div>
+                      <div class="rounded-[5px] bg-[#26B82C] relative flex flex-row justify-center p-[6px_40px_8px_40px] box-sizing-border">
+                        <span class="break-words font-['Poppins'] font-medium text-[20px] leading-[1.355] text-[#FFFFFF]">
+                          Sewa
+                        </span>
+                      </div>
+           
+                  </div>
+              </div>
+            </div>
+          </div>
+    </div>
+
+    
+
+
+</main>
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-0Tg1wZTC8whrTzAmBx8hH1/HUZv+jEjKAwiVITMNRE8V3zVZ/sS8kB6I95tZOy0k" crossorigin="anonymous"></script>
+
+    </body>
+</html>
