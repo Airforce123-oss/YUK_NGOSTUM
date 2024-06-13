345b16717d4a411cf678e48345e2c8dde0d77884
Fadel442 <fadelmuhammad4402@gmail.com>
2024-06-13 14:05:15 +0700

    nav rev

--- resources/views/toko/guest-preview-toko.blade.php
+++ resources/views/toko/guest-preview-toko.blade.php
@@ -13,18 +13,7 @@
     </div>
 
     <ul class="md:flex md:items-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto">
-        <li class="mx-4 my-6 md:my-0">
-            <a href="#" class="text-xl hover:text-white duration-500" style="color: white !important; text-decoration: none; font-size:20px;">Cari Costume Apa?</a>
-        </li>
-        <li class="mx-4 my-6 md:my-0">
-            <a href="#" class="text-xl hover:text-white duration-500"style="color: white !important; text-decoration: none; font-size:20px;">Notifikasi</a>
-        </li>
-        <li class="mx-4 my-6 md:my-0">
-            <a href="#" class="text-xl hover:text-white duration-500"style="color: white !important; text-decoration: none; font-size:20px;">Lainnya</a>
-        </li>
-        <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white hover:bg-black-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="showDialog()" type="button" style="font-size:20px;">
-            Profil
-            </button>
+        
 </nav>
 
 
